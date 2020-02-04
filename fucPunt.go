3d45324d9ff29679e28d359cf0c0c37c6c702ac6
rodrigo <rsantiago643@gmail.com>
2020-02-04 16:06:30 -0600

    Ejemplo funciones ante del fix

---
+++ fucPunt.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+//puntero en funcion
+func crearPuntero() *float64 {
+	var myFloat = 89.6
+	return &myFloat
+}
+
+//funcion con el parametro del puntero
+func impriirPuntero(miBoleanoPuntero *bool) {
+	fmt.Println(*miBoleanoPuntero)
+	fmt.Println(miBoleanoPuntero)
+}
+
+// funcción doble usando funciones
+func doble(numero int) {
+	numero *= 2
+}
+
+func main() {
+	//puntero en función
+	var myFloatPuntero *float64 = crearPuntero()
+	fmt.Println(*myFloatPuntero)
+	//funcion con el parametro del puntero
+	var miBoleano bool = true
+	fmt.Println(&miBoleano)
+	impriirPuntero(&miBoleano)
+	//doble funcion
+	cantidad := 2
+	doble(cantidad)
+	//se ejecuta la funcion sin afectar la variable
+	fmt.Println(cantidad)
+}
